a752ceafa1c8504b19327049904f7dda90796bea
Sandra Ptaszek <ptaszek.sandra@gmail.com>
2016-08-06 10:53:36 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var main = function(){
+	var apiUrl =  'http:api.nbp.pl/api';
+
+	$.ajax({
+		url: apiUrl + '/exchangerates/tables/C?format=JSON',
+		method: 'GET',
+	}) .then(function(data) {
+		printTable(data[0].rates);		
+		console.log(data[0].effectiveDate);
+	});
+
+	function printTable(rates) {
+		for (var i = 0; i<rates.length; i++){
+			$('#exchange').append('<tr><td>' + rates[i].currency 
+				+ '</td><td>' + rates[i].ask + '</td></tr>')
+		}
+	};
+};
+
+$(document).ready(main);
